1c8a905b36072cf1ede8bbc6e68c4b70d5d5f8fc
EnoF <andyt@live.nl>
2014-11-14 11:59:23 +0100

    Restcall Widget specs

---
+++ test/unit/app/widgets/restCallWidgetSpecs.js
@@ -0,0 +1,22 @@
+// con-rest
+// Version: 0.0.1
+//
+// Author: Andy Tang
+// Fork me on Github: https://github.com/EnoF/con-rest
+(function restCallWidgetSpecs() {
+    'use strict';
+
+    describe('restCall widget specs', function restCallWidgetSpecs() {
+        it('should load the restCall when an id has been provided', function loadRestCall() {
+
+        });
+
+        it('should directly enter editing mode when no id and rest call model is provided', function startEditing() {
+
+        });
+
+        it('should display the provided rest call', function providedCall() {
+
+        });
+    });
+}());
