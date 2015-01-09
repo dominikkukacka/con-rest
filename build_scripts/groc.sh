c23deb7cc02003ce06a209cb109e2b017f897449
EnoF <andyt@live.nl>
2015-01-09 14:13:57 +0100

    added travis-ci support

---
+++ build_scripts/groc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+if [ $TRAVIS_BRANCH == "master" ]; then
+    git checkout master
+    grunt groc:dist
+    rm -rf app/**/**/*.* app/**/*.js app/*.js test .jshintrc bower.json .bowerrc index.js package.json
+    git config user.name "Travis CI"
+    git config user.email "andortang+travis.ci@gmail.com"
+    git add -A
+    git commit -m "Automatically updated the groc documentation via drone.io"
+    git push -f --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+fi
